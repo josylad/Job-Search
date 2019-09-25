27db426d00135d5572aabe842bfa2bb23aac362f
Arnold Kibira <arnold.ril3y@gmail.com>
2019-09-25 16:31:04 +0300

    create form field for user to search for cheap flights

--- app/flights/forms.py
+++ app/flights/forms.py
@@ -1,20 +1,12 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, SubmitField, DateField
-from wtforms.validators import (DataRequired, Length, Email, EqualTo,
-                                ValidationError)
-from app.models import User
-from flask_login import current_user
-from flask_datepicker import datepicker
+from wtforms.validators import DataRequired, Length
+from wtforms.fields.html5 import DateField
 
 
 class FlightForm(FlaskForm):
     origin = StringField("Origin", validators=[DataRequired()])
     destination = StringField("Destination", validators=[DataRequired()])
-    depart_date = DateField('Departure Date', validators=[DataRequired()])
-    return_date = DateField('Return Date', validators=[DataRequired()])
-    submit = SubmitField('Buy Now', validators=[DataRequired()])
-
-
-
-
+    depart_date = DateField('Departure Date', format='%Y-%m-%d')
+    return_date = DateField('Return Date', format='%Y-%m-%d')
+    submit = SubmitField('Buy Now')
