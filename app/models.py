da1b66b03e758b56dd0136862f1de50519a69514
Arnold Kibira <arnold.ril3y@gmail.com>
2019-09-25 16:27:22 +0300

    create User modes

--- app/models.py
+++ app/models.py
@@ -15,65 +15,33 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(225), unique=True, nullable=False)
     fullname = db.Column(db.String(), nullable=False)
     email = db.Column(db.String(255), unique= True, nullable= False, index = True)
-    bio = db.Column(db.String(255))
-    image = db.Column(db.String(225), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    facebook = db.Column(db.String())
-    twitter = db.Column(db.String())
-    github = db.Column(db.String())
-    linkedin = db.Column(db.String())
-    posts = db.relationship('Post', backref='author', lazy=True)
-    comments = db.relationship('Comment', backref='author', lazy=True)
 
-    
     def __repr__(self):
         return f'User {self.username}'
-    
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    posted_date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    image = db.Column(db.String(225), default='default.jpg')
-    category = db.Column(db.String(255), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    comments = db.relationship('Comment', backref='post', lazy=True)
 
-    
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.posted_date}', '{self.category}')"
-    
-
-class Comment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    fullname = db.Column(db.String(255))
-    comment = db.Column(db.Text, nullable=False)
-    posted_date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
-
-    
-    def save_comment(self):
-        db.session.add(self)
-        db.session.commit()
-
-    @classmethod
-    def get_comment(cls,id):
-        comments = Comment.query.filter_by(post_id=id).all()
-        return comments
-    
-    def __repr__(self):
-        return f"Comment('{self.comment}', '{self.posted_date}')"
-    
-    
-class Quote:
+class Flight:
     '''
-    Quote class to define Quote Objects
+    Flight class to define Flight Objects
     '''
 
-    def __init__(self, id, author, quote, permalink):
-        self.id = id
-        self.author = author
-        self.quote = quote
-        self.permalink = permalink
+    def __init__(self, price, airline, flight_number, departure_at, return_at, 
+                 expires_at):
+        self.price = price
+        self.airline = airline
+        self.flight_number = flight_number
+        self.departure_at = departure_at
+        self.return_at = return_at
+        self.expires_at = expires_at
+
+
+class Hotel:
+    '''
+    Hotel class to define Hotel Objects
+    '''
+
+    def __init__(self, label, locationName, fullName):
+        self.label = label
+        self.locationName = locationName
+        self.fullName = fullName
