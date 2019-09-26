3e225b3632a32acf56e30c3e1bc9ffbdb45218f8
Joseph Adediji <josylado@gmail.com>
2019-09-26 12:21:07 +0300

    Update for API support

--- app/main/views.py
+++ app/main/views.py
@@ -1,28 +1,30 @@
 from flask import render_template, request, Blueprint
-from app.models import Post
-from app.request import get_quote
+from app.models import Flight, Hotel
+from app.request import get_flights, get_flightgroup
 
 main = Blueprint('main', __name__)
 
-quotes = get_quote()
+# get_flights = get_flights()
+
+
 @main.route("/")
 @main.route("/home")
 def home():
-    
+    flights = get_flights()
+    if flights is None:
+        abort(404)
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.posted_date.desc()).paginate(page=page, per_page=7)
     # categories = Post.query.filter_by(category=post.category).all()
-    myposts = Post.query.order_by(Post.posted_date.desc())
-    return render_template('index.html', posts=posts, myposts=myposts ,quotes=quotes)
-
+    # print (flights[0])
+    print(flights)
+    return render_template('index.html',flights=flights)
 
 @main.route("/about")
 def about():
-    myposts = Post.query.order_by(Post.posted_date.desc())
-    return render_template('about.html', title='About', myposts=myposts, quotes=quotes)
+    return render_template('about.html')
 
 
 @main.route("/subscribe")
 def subscribe():
-    return render_template('subscribe.html', title='Subscription', quotes=quotes)
+    return render_template('subscribe.html')
 
