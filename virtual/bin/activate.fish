ee79205bf691588b9062337e78c3d94ecbebb784
happiness <happinesskawinzi@gmail.com>
2019-09-27 11:58:35 +0300

    commiting templates

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/riley/Flask/Travel-Bookings/virtual"
+set -gx VIRTUAL_ENV "/home/hapiness/Ip/Job-Search/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
