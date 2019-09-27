0efa0e8be80f1da921bfbc198a4b3aed0f79fbb1
Arnold Kibira <arnold.ril3y@gmail.com>
2019-09-27 09:14:16 +0300

    create the job search form

--- app/hotels/forms.py
+++ app/hotels/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import TextField
+from wtforms import TextField, SubmitField
 from wtforms.validators import DataRequired
 
 
-class HotelForm(FlaskForm):
+class JobSearchForm(FlaskForm):
     city = TextField("City", validators=[DataRequired()])
+    keyword = TextField('Keyword', validators=[DataRequired()])
+    search = SubmitField('Search')
